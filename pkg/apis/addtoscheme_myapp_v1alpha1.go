4145f4e3213ade5a037d4bae7d41fae56a322e2d
huzefa <h.h@gmail.com>
2020-07-08 13:18:58 +0000

    api files

---
+++ pkg/apis/addtoscheme_myapp_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"github.com/huzefa51/myapp-operator/pkg/apis/myapp/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
---
+++ pkg/apis/myapp/group.go
@@ -0,0 +1,6 @@
+// Package myapp contains myapp API versions.
+//
+// This file ensures Go source parsers acknowledge the myapp package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package myapp
