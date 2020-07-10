e9a592bb910f5cc2e2ad9d7419db0358b840dbb2
huzefa51 <huzefa.hamdard53@gmail.com>
2020-07-10 10:43:07 +0900

    Update myapp_controller.go

--- pkg/controller/myapp/myapp_controller.go
+++ pkg/controller/myapp/myapp_controller.go
@@ -213,3 +213,39 @@ func newPodForCR(cr *appv1alpha1.MyApp) *corev1.Pod {
             },
         }
     }
+
+// NewServiceSyncer returns a new sync.Interface for reconciling Tomcat Service
+func NewServiceSyncer(tc *tomcat.Tomcat, c client.Client, scheme *runtime.Scheme) syncer.Interface {
+	objLabels := tc.ComponentLabels(tomcat.TomcatDeployment)
+
+	obj := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tc.Name,
+			Namespace: tc.Namespace,
+		},
+	}
+
+	return syncer.NewObjectSyncer("Service", tc.Unwrap(), obj, c, scheme, func(existing runtime.Object) error {
+		out := existing.(*corev1.Service)
+		out.Labels = labels.Merge(labels.Merge(out.Labels, objLabels), controllerLabels)
+
+		selector := tc.TomcatServerPodLabels()
+		if !labels.Equals(selector, out.Spec.Selector) {
+			if out.ObjectMeta.CreationTimestamp.IsZero() {
+				out.Spec.Selector = selector
+			} else {
+				return fmt.Errorf("service selector is immutable")
+			}
+		}
+
+		if len(out.Spec.Ports) != 1 {
+			out.Spec.Ports = make([]corev1.ServicePort, 1)
+		}
+
+		out.Spec.Ports[0].Name = "http"
+		out.Spec.Ports[0].Port = *tc.Spec.ServicePort
+		out.Spec.Ports[0].TargetPort = intstr.FromInt(tomcat.TomcatHTTPPort)
+
+		return nil
+	})
+}
